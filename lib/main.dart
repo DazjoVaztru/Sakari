2e04c28d9ce5c08b5af03a7a4ab0bd5317396dcc
David Vazquez <vazquezjosuedavid.2018@gmail.com>
2026-04-06 23:44:09 -0600

    Añadir idioma

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart'; // NUEVO IMPORT PARA ACCESIBILIDAD
 import 'screens/login_screen.dart';
 
 void main() {
@@ -11,8 +12,22 @@ class DentalConnectApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // ACCESIBILIDAD: Título que lee el lector de pantalla al cambiar entre aplicaciones
       title: 'Dental Connect',
       debugShowCheckedModeBanner: false,
+
+      // ACCESIBILIDAD: Forzar el idioma a español para que TalkBack/VoiceOver
+      // pronuncie todo correctamente y traduzca etiquetas nativas (ej. "Atrás", "Cerrar")
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('es', 'MX'), // Español de México
+        Locale('es', 'ES'), // Español genérico
+      ],
+
       theme: ThemeData(
         primaryColor: const Color(0xFF0277BD),
         scaffoldBackgroundColor: const Color(0xFFE1F5FE),
